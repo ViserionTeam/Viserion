56673023f45ac9763a0e8d0e8004b2f49672d326
Willian Taiguara <willianwt@gmail.com>
2019-10-25 18:55:01 -0300

    update

---
+++ mobile_expo/src/routes.js
@@ -0,0 +1,11 @@
+import { createSwitchNavigator, createAppContainer } from 'react-navigation';
+
+import Main from './pages/Main'; 
+
+const Routes = createAppContainer(
+    createSwitchNavigator({
+        Main
+    })
+);
+
+export default Routes; 
